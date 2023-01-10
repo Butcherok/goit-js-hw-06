9f48c2f0a10edc1c608908ffae27d25841c20e4f
Butcherok <butcherok231@gmail.com>
2023-01-10 16:52:57 +0200

    1.02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsEl = ingredients.map(ingredient => {
+  const makeEl = document.createElement(`li`);
+  makeEl.textContent = ingredient;
+  makeEl.classList.add(`item`);
+  return makeEl;
+});
+document.querySelector(`#ingredients`).append(...ingredientsEl);
