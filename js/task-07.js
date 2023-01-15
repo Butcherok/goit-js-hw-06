10305c03e07bcdaa78ec26f6c0fc0dc7857c1ee5
Butcherok <butcherok231@gmail.com>
2023-01-16 00:18:18 +0200

    1.07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const statusRange = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+statusRange.addEventListener('change', onChangeRange);
+
+function onChangeRange (event) {
+    statusRange.range = event.currentTarget.value;
+    text.style.fontSize = `${event.currentTarget.value}px`;
+};
