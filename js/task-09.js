646d0cea8ae71eccf9a0394c522918352db9326b
Butcherok <butcherok231@gmail.com>
2023-01-16 22:02:00 +0200

    1.09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const objBody = document.querySelector('body');
+const objButton = document.querySelector('button.change-color');
+const textColor = document.querySelector('span.color');
+
+objButton.addEventListener('click', onChangeColor);
+
+function onChangeColor (event) {
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  objBody.style.backgroundColor = `${getRandomHexColor()}`;
+  textColor.textContent = `${getRandomHexColor()}`;
+};
