60b14ffb2ba3044fa643e06ed028cb03de52f8f2
Butcherok <butcherok231@gmail.com>
2023-01-17 17:11:43 +0200

    1.19

--- js/task-08.js
+++ js/task-08.js
@@ -1,37 +1,16 @@
-// Напиши скрипт управління формою логіна.
-
-// Обробка відправлення форми form.login-form
-// повинна відбуватися відповідно до події submit.
-
-// Під час відправлення форми сторінка
-// не повинна перезавантажуватися.
-
-// Якщо у формі є незаповнені поля,
-// виводь alert з попередженням про те,
-// що всі поля повинні бути заповнені.
-
-// Якщо користувач заповнив усі поля і відправив форму,
-// збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості,
-// а значення поля - значенням властивості.
-// Для доступу до елементів форми використовуй властивість elements.
-
-// Виведи об'єкт із введеними даними в консоль
-// і очисти значення полів форми методом reset.
-
 const form = document.querySelector('form.login-form');
 
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
     event.preventDefault()
-    const outputInfo = {};
     const formData = new FormData(event.currentTarget)
         if (event.currentTarget.elements.email.value === ''
         ||  event.currentTarget.elements.password.value === ''
         ) {
             alert(`You must fill in all fields!!!`)
-        } 
-            console.log({event.currentTarget.elements.email.name: event.currentTarget.elements.email.value, event.currentTarget.elements.password.name: event.currentTarget.elements.password.value})
-        
+        } else {
+            console.log({email: event.currentTarget.elements.email.value, name: event.currentTarget.elements.password.value})
+    }
     event.currentTarget.reset();
 }
